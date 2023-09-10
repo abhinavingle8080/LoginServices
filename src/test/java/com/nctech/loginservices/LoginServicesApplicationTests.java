8d0d6052a19cd42c9beeff1a9ebd71c2f135e584
pazavur <pazavurdapirilas@gmail.com>
2023-09-10 13:31:07 +0530

    Initial Commit

---
+++ src/test/java/com/nctech/loginservices/LoginServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nctech.loginservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
